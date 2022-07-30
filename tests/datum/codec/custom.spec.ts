a9e7fb36a242ef6838a2aa97db581fba692ba33c
Jason Kuhrt <jasonkuhrt@me.com>
2022-07-30 10:42:59 -0400

    more tests

--- tests/datum/codec/custom.spec.ts
+++ tests/datum/codec/custom.spec.ts
@@ -22,83 +22,119 @@ it(`adds custom encoders to the controller`, () => {
 })
 
 describe(`encoder`, () => {
-  describe(`inputs`, () => {
-    it(`1: data instance`, () => {
-      const A = Alge.datum($A)
-        .schema({ a: z.number() })
-        .codec(`foo`, {
-          to: (data) => `${data._tag}:${data.a}`,
-          from: () => ({ a: 1 }),
-        })
-        .done()
-      expect(A.to.foo(A.create({ a: 1 }))).toEqual(`A:1`)
+  describe(`builder`, () => {
+    describe(`inputs`, () => {
+      it(`1: data instance`, () => {
+        type A = Alge.InferDatum<typeof A>
+        const A = Alge.datum($A)
+          .schema({ a: z.number() })
+          .codec(`foo`, {
+            to: (data) => {
+              expectType<A>(data)
+              return `${data._tag}:${data.a}`
+            },
+            from: () => ({ a: 1 }),
+          })
+          .done()
+        expect(A.to.foo(A.create({ a: 1 }))).toEqual(`A:1`)
+      })
+    })
+    describe(`return`, () => {
+      it(`string`, () => {
+        Alge.datum($A)
+          .schema({ a: z.number() })
+          .codec(`foo`, {
+            // @ts-expect-error Must be a string
+            to: () => 1,
+            from: () => ({ a: 1 }),
+          })
+      })
     })
   })
-  describe(`return`, () => {
-    it(`string`, () => {
-      Alge.datum($A)
+  describe(`controller`, () => {
+    it(`input: requires data`, () => {
+      type A = Alge.InferDatum<typeof A>
+      const A = Alge.datum($A)
         .schema({ a: z.number() })
-        .codec(`foo`, {
-          // @ts-expect-error Must be a string
-          to: () => 1,
-          from: () => ({ a: 1 }),
-        })
+        .codec(`foo`, { to: () => ``, from: () => ({ a: 1 }) })
+        .done()
+      expectType<(data: A) => string>(A.to.foo)
     })
   })
 })
 
 describe(`decoder`, () => {
-  describe(`inputs`, () => {
-    it(`1: stringified value`, () => {
-      const A = Alge.datum($A)
-        .schema({ a: z.number() })
-        .codec(`string`, {
-          to: () => ``,
-          from: (string) => ({ a: Number(string) }),
-        })
-        .done()
-      expect(A.from.string(`100`)).toMatchObject({ a: 100 })
+  describe(`builder`, () => {
+    describe(`inputs`, () => {
+      it(`1: stringified value`, () => {
+        const A = Alge.datum($A)
+          .schema({ a: z.number() })
+          .codec(`string`, {
+            to: () => ``,
+            from: (string) => ({ a: Number(string) }),
+          })
+          .done()
+        expect(A.from.string(`100`)).toMatchObject({ a: 100 })
+      })
+      it(`2: context`, () => {
+        const A = Alge.datum($A)
+          .schema({ a: z.array(z.any()) })
+          .codec(`string`, {
+            to: () => ``,
+            from: (_, context) => {
+              expectType<$A>(context.name)
+              expectType<z.AnyZodObject>(context.schema)
+              return { a: [context.name, context.schema] }
+            },
+          })
+          .done()
+        expect(A.from.string(``)).toMatchObject({ a: [$A, A.schema] })
+      })
     })
-    it(`2: context`, () => {
-      const A = Alge.datum($A)
-        .schema({ a: z.array(z.any()) })
-        .codec(`string`, {
-          to: () => ``,
-          from: (_, context) => {
-            expectType<$A>(context.name)
-            expectType<z.AnyZodObject>(context.schema)
-            return { a: [context.name, context.schema] }
-          },
-        })
-        .done()
-      expect(A.from.string(``)).toMatchObject({ a: [$A, A.schema] })
+    describe(`return`, () => {
+      it(`value must adhere to schema`, () => {
+        Alge.datum($A)
+          .schema({ a: z.number() })
+          .codec(`foo`, {
+            to: () => ``,
+            // @ts-expect-error return does not conform to schema.
+            from: () => ({ b: 1 }),
+          })
+          .codec(`bar`, {
+            to: () => ``,
+            // @ts-expect-error return does not conform to schema.
+            from: () => ({}),
+          })
+      })
+      it(`null if decoding not possible`, () => {
+        const A = Alge.datum($A)
+          .schema({ a: z.number() })
+          .codec(`string`, {
+            to: () => ``,
+            from: (string) => (string === `bad` ? null : { a: Number(string) }),
+          })
+          .done()
+
+        expect(A.from.string(`bad`)).toBeNull()
+      })
     })
   })
-  describe(`return`, () => {
-    it(`value must adhere to schema`, () => {
-      Alge.datum($A)
-        .schema({ a: z.number() })
-        .codec(`foo`, {
-          to: () => ``,
-          // @ts-expect-error return does not conform to schema.
-          from: () => ({ b: 1 }),
-        })
-        .codec(`bar`, {
-          to: () => ``,
-          // @ts-expect-error return does not conform to schema.
-          from: () => ({}),
-        })
+  describe(`controller`, () => {
+    const A = Alge.datum($A)
+      .schema({ a: z.number() })
+      .codec(`string`, {
+        to: () => ``,
+        from: () => null,
+      })
+      .done()
+    it(`input: requires string`, () => {
+      // @ts-expect-error Requires string
+      A.from.string()
+      // @ts-expect-error Requires string
+      A.from.string(1)
     })
-    it(`null if decoding not possible`, () => {
-      const A = Alge.datum($A)
-        .schema({ a: z.number() })
-        .codec(`string`, {
-          to: () => ``,
-          from: (string) => (string === `bad` ? null : { a: Number(string) }),
-        })
-        .done()
-
-      expect(A.from.string(`bad`)).toBeNull()
+    it(`has orThrow variant which throws when null is returned`, () => {
+      expect(() => A.from.stringOrThrow(``)).toThrowError()
     })
   })
 })
