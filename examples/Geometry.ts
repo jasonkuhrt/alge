392ff7e1ea49bc26d2c6a8e4c32bfc83429db951
Jason Kuhrt <jasonkuhrt@me.com>
2022-08-02 11:21:07 -0400

    chore(docs): improve geometry example

--- examples/Geometry.ts
+++ examples/Geometry.ts
@@ -3,18 +3,57 @@ import { z } from 'zod'
 
 const Length = z.number().positive()
 
-export const Shape = Alge.data(`Shape`)
+const Shape = Alge.data(`Shape`)
   .variant(`Rectangle`)
   .schema({
     width: Length,
     height: Length,
   })
-  .variant(`circle`)
+  .codec(`graphic`, {
+    to: (rectangle) => `${rectangle.width}x${rectangle.height}`,
+    from: (graphic) => {
+      const match = graphic.match(/(\d+)x(\d+)/)
+      if (!match) return null
+      const [_, width, height] = match
+      return {
+        width: Number(width),
+        height: Number(height),
+      }
+    },
+  })
+  .variant(`Circle`)
   .schema({
     radius: Length,
+  })
+  .codec(`graphic`, {
+    to: (circle) => `(---|${circle.radius})`,
+    from: (graphic) => {
+      const match = graphic.match(/\(---|(\d+)\)/)
+      if (!match) return null
+      const [_, radius] = match
+      return {
+        radius: Number(radius),
+      }
+    },
   })
   .variant(`Square`)
   .schema({
     size: Length,
   })
+  .codec(`graphic`, {
+    to: (square) => `[${square.size}]`,
+    from: (graphic) => {
+      const match = graphic.match(/[(\d+)]/)
+      if (!match) return null
+      const [_, size] = match
+      return {
+        size: Number(size),
+      }
+    },
+  })
   .done()
+
+const circle = Shape.Circle.create({ radius: 10 })
+
+console.log({ circle })
+// { _tag: 'Circle', radius: 10 }
