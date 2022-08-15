e31a4ce2e199f439042ed85e98eabf86cc901842
Jason Kuhrt <jasonkuhrt@me.com>
2022-08-15 19:48:30 +0200

    chore(docs): simplify example

--- examples/GeometryDataSimple.ts
+++ examples/GeometryDataSimple.ts
@@ -1,6 +1,10 @@
 import { Alge } from '../src/index.js'
 import { z } from 'zod'
 
+/**
+ * Builder
+ */
+
 const Length = z.number().positive()
 
 export const Shape = Alge.data(`Shape`, {
@@ -16,32 +20,32 @@ export const Shape = Alge.data(`Shape`, {
   },
 })
 
+/**
+ * Controller
+ */
+
 const circle = Shape.Circle.create({ radius: 11 })
-console.log(circle)
-// { _tag: 'Circle', radius: 10 }
-
 const square = Shape.Square.create({ size: 13 })
-console.log(square)
-// { _tag: 'Circle', radius: 10 }
-
 const shape = Math.random() > 0.5 ? circle : square
+Shape.Square.is(circle)
+Shape.Circle.is(circle)
+Shape.Circle.to.json(circle)
+Shape.Circle.from.json(`{ "radius" 11, "_tag": "Circle" }`)
+Shape.to.json(shape)
+Shape.from.json(`{ "width" 17, "height": 22 "_tag": "Square" }`)
 
-const tenOrNil = Alge.match(shape)
+/**
+ * Matcher
+ */
+
+Alge.match(shape)
   .Circle({ radius: 10 }, () => true)
   .else(null)
 
-console.log(tenOrNil)
-// null
-
-const result = Alge.match(shape)
+Alge.match(shape)
   .Circle({ radius: 11 }, () => `big circle`)
   .Circle({ radius: 1 }, () => `little circle`)
   .Circle(() => `other circle`)
   .Square({ size: 13 }, () => `unlucky square`)
   .Square(() => `a good square`)
   .done()
-
-console.log(result)
-// Random:
-// 'big circle'
-// 'unlucky square'
